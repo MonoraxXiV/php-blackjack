b1cd48ea911ba852c0573e6367b339b1603c0396
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-12 14:18:56 +0200

    finished step three

--- player.php
+++ player.php
@@ -5,4 +5,17 @@ class Player {
 
     private array $cards=[];
     private bool $lost= false;
+
+    public function hit(){
+
+    }
+    public function surrender(){
+
+    }
+    public function getScore(){
+
+    }
+    public function hasLost(){
+
+    }
 }
