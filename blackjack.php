18dad31b53be17f48adb7c8b03396231cde7c8ed
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-12 14:25:20 +0200

    finished step 4-6 in creating the base classes

---
+++ blackjack.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+class Blackjack {
+    private $player;
+    private $dealer;
+    private $deck;
+
+    private function getPlayer(){
+
+    }
+    private function setPlayer(){
+
+    }
+
+}
+
