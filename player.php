366115e5a9faeb9c46e33fd3c3c196976300ffca
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-12 15:26:18 +0200

    attempt to catch  and drawing 2 cards

--- player.php
+++ player.php
@@ -1,10 +1,19 @@
 <?php
 declare(strict_types=1);
 
+require 'blackjack.php';
+require 'Deck.php';
 class Player {
 
     private array $cards=[];
     private bool $lost= false;
+
+
+    public function __construct($deck)
+    {
+        //getDeck()?
+        $deck->drawCard(2);
+    }
 
     public function hit(){
 
@@ -18,4 +27,5 @@ class Player {
     public function hasLost(){
 
     }
+
 }
