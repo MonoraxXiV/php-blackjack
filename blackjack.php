f3f63e65dae474dc72aac2955a7add70fdc542ef
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-12 15:04:28 +0200

    step 7 somewhat complete, not sure if player is initiated

--- blackjack.php
+++ blackjack.php
@@ -1,16 +1,27 @@
 <?php
 declare(strict_types=1);
+require 'player.php';
+require 'Deck.php';
 
 class Blackjack {
-    private $player;
-    private $dealer;
-    private $deck;
+    private  $player;
+    private  $dealer;
+    private  $deck;
+
+
 
     public function getPlayer(){
 
     }
-    public function setPlayer(){
+    public function getDealer(){
 
+    }
+    public function __construct($player, $dealer)
+    {
+        $this->player=$player;
+        $this->dealer=$dealer;
+        $deck=new Deck();
+        $deck->shuffle();
     }
 
 }
