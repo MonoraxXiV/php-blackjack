bf4e5dcbcea630bfe1e5391e0e122a20bc5c2b4f
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-13 15:50:28 +0200

    changed from variable deck to this-> deck

--- blackjack.php
+++ blackjack.php
@@ -11,6 +11,16 @@ class Blackjack {
     public function getPlayer(){
     return $this->player;
     }
+
+    /**
+     * @param Player $player
+     */
+    public function setPlayer(Player $player): void
+    {
+        $this->player = $player;
+    }
+
+
     public function getDealer(){
     return $this->dealer;
     }
@@ -20,11 +30,14 @@ class Blackjack {
         // old attempt at Instantiate
         //$this->player=$player;
         //$this->dealer=$dealer;
-        $deck=new Deck();
-        $deck->shuffle();
-        $this->player= new Player($deck);
-        $this->dealer= new Player($deck);
+       $this->deck=new Deck();
+        $this->deck->shuffle();
+        $this->player= new Player($this->deck);
+        $this->dealer= new Player($this->deck);
 
+    }
+    public function getDeck(){
+        return $this->deck;
     }
 
 }
