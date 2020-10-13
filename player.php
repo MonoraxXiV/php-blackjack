8ed64acbd779076bfafef7003d0f36b83dd56fbd
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-13 15:52:03 +0200

    changed the foreach loop, edited haslost method, trying to get the if statement to work so I can unset

--- player.php
+++ player.php
@@ -1,30 +1,51 @@
 <?php
 declare(strict_types=1);
 
-class Player {
+class Player
+{
 
-    private array $cards=[];
-    private bool $lost= false;
+    private array $cards = [];
+    private bool $lost = false;
 
 
     public function __construct(Deck $deck)
     {
-        array_push($this->cards);
-        $this->cards[]=$deck->drawCard();
+        $this->cards=[];
+            array_push($this->cards,$deck->drawCard(),$deck->drawCard());
 
     }
 
-    public function hit(){
-        return $this->cards;
-    }
-    public function surrender(){
-    return $lost=true;
-    }
-    public function getScore(){
+    public function hit( $deck)
+    {
+
+       array_push($this->cards,$deck->drawCard());
+
 
     }
-    public function hasLost(){
-        return $this->lost;
+
+    public function surrender()
+    {
+        return $lost = true;
+
+    }
+
+    public function getScore()
+    { $score=0;
+    foreach ($this->cards AS $card){
+
+       $score+=$card->getValue();
+        //echo $card->getUnicodeCharacter(true);
+
+
+    }
+        return $score;
+    }
+
+    public function hasLost()
+    {
+          $this->lost=true;
+
+
     }
 
 }
