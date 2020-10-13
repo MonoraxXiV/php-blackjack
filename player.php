300f7a2c4ebcb0323aa17742208bba0ebcd7a0ac
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-13 09:42:29 +0200

    attempt at defining the methods

--- player.php
+++ player.php
@@ -7,23 +7,24 @@ class Player {
     private bool $lost= false;
 
 
-    public function __construct($deck)
+    public function __construct(Deck $deck)
     {
-        //getDeck()?
-        $deck->drawCard(2);
+        array_push($this->cards);
+        $this->cards[]=$deck->drawCard();
+
     }
 
     public function hit(){
-
+        return $this->cards;
     }
     public function surrender(){
-
+    return $lost=true;
     }
     public function getScore(){
 
     }
     public function hasLost(){
-
+        return $this->lost;
     }
 
 }
